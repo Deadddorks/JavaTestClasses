ec60c44763c5d615a052c61b36c3a84cb09a9861
Deadddorks <rudnicki.kalin@uwlax.edu>
2017-11-27 13:49:00 -0600

    Added Person

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,50 @@
+package person;
+
+// ~~~~~~~~~~ Imports ~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+public class Person
+{
+	
+	// ----- Label -----
+	// ~~~~~~~~~~ Constants ~~~~~~~~~~
+	
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// ~~~~~~~~~~ Variables ~~~~~~~~~~
+	private String firstName;
+	private String lastName;
+	private int age;
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+	public Person(final String firstName, final String lastName, final int age)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "{Person} {firstName: ["+firstName+"], lastName: ["+lastName+"], age: ["+age+"]}";
+	}
+}
